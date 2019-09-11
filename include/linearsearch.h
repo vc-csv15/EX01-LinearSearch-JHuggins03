a8615092692a1aa2a9ad3683d08fd4522c6ee1a6
vscilab <Scilab01@vcccd.edu>
2019-09-11 14:29:56 -0700

    Commit File Test

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -14,7 +14,6 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
                 template <class T>
                 int64_t find(const T& value, T array[], size_t size) {
-                    // put your code here
                     if(array == nullptr)
                         {return -1;}
                     for(size_t i= 0; i< size; i++)
